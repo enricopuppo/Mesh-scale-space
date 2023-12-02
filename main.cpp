1d54598c374d3d9f3b593b45e0550f89d9bbf01a
Enrico Puppo <enrico.puppo@unige.it>
2023-12-02 13:12:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ using namespace cinolib;
 
 //:::::::::::::::::::::::::::: GLOBAL VARIABLES (FOR GUI) ::::::::::::::::::::::::::::::
 // Global state
+enum field_methods {GAUSS, MEAN, L_EIGEN, COORDX, COORDY, COORDZ, RANDOM};
+
 struct State {
   bool MESH_IS_LOADED, FIELD_IS_PRESENT, SCALE_SPACE_IS_PRESENT;
   // Input
@@ -21,12 +23,15 @@ struct State {
   uint nverts;                    // its #vertices
   vector<vector<uint>> VV;        // its VV relation
   // Output
-  ScalarField phi;                // the base field
   vector<vector<double>> fields;  // the discrete scale-space
   uint nlevels;                   // # levels in the scale-space 
   vector<float*> clamp_limits;    // clamp limits for field visualization at all levels
-  // Processing
+  // Field generation
   Eigen::VectorXd f;              // base field on m
+  int current_field_method;       // current method to generate the field
+  vector<string> field_method_labels; // labels for the combo box
+  int max_eigenfunctions, selected_eigenfunction; // parameters for eigenfunction generator
+  // Scale space
   double t_step;                  // base time step of diffusion
   double mult;                    // multiplicator/stride of time step in diffusion
   vector<vector<int>> critical;   // critical points at all levels
@@ -41,7 +46,12 @@ struct State {
     MESH_IS_LOADED = FIELD_IS_PRESENT = SCALE_SPACE_IS_PRESENT = false;
     show_sf = show_cp = show_wf = false;
     show_m = true;
-
+    // field generation
+    current_field_method = GAUSS;
+    field_method_labels = {"Gaussian curvature", "Mean curvature", "Laplacian eigenfunction", "Coordinate x", "Coordinate y", "Coordinate z", "Random"};
+    max_eigenfunctions = 100;
+    selected_eigenfunction = 1;
+    // view
     selected_entry = 0;
     point_multiplier = 1.0;
   }
@@ -84,7 +94,7 @@ void Save_scale_space(const State &gs)
   f.close();
 }
 
-
+//-------------
 // functions to  render vertices as spheres
 inline void draw_points(const vector<DrawableSphere> &cp, GLcanvas &gui) {
   for (auto &point : cp) //if (point.radius > 0) 
@@ -119,6 +129,16 @@ inline void reset_points(vector<DrawableSphere> &cp)
 }
 
 // ------------
+
+const char* wrap_strings(const vector<string> &s)
+{
+  string ss;
+  for (int i=0;i<s.size();i++) ss = ss + "\0" + s[i];
+  ss += "\0";
+  return ss.c_str();  
+}
+
+// ------------
 ScalarField Clamp_and_rescale_field(const vector<double> &f, const float cl[])
 // f field is in [0,1] both in input and in output
 // cl clamp limits: clamp f in [cl[0],cl[1]] and rescale it to [0,1]
@@ -133,134 +153,6 @@ ScalarField Clamp_and_rescale_field(const vector<double> &f, const float cl[])
   return sf;
 }
 
-//::::::::::::::::::::::::::::::::: GUI ::::::::::::::::::::::::::::::::::::::::::::::::
-
-GLcanvas Init_GUI()
-{
-  GLcanvas gui(1500,700);
-  gui.side_bar_width = 0.25;
-  gui.show_side_bar = true;
-  return gui;
-}
-
-void Setup_GUI_Callbacks(GLcanvas & gui, State &gs)
-{
-  gui.callback_app_controls = [&]() {
-    ImGui::SeparatorText("Files");
-    if (ImGui::Button("Load mesh")) {
-      if (gs.MESH_IS_LOADED) {
-        ImGui::OpenPopup("Load mesh?");
-      } else {
-        Load_mesh(gs);
-      }
-    }
-    // Modal popup for loading files
-    ImVec2 center = ImGui::GetMainViewport()->GetCenter();
-    ImGui::SetNextWindowPos(center,ImGuiCond_Appearing,ImVec2(0.5f,0.5f));
-    if (ImGui::BeginPopupModal("Load mesh?", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse))
-    {
-      ImGui::Text("All data structures will be reset - Load mesh anyway?\n\n");
-      ImGui::Separator();           
-      static bool dont_ask_me_next_time = false;
-      ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0,0));
-      ImGui::Checkbox("Don't ask me next time", &dont_ask_me_next_time);
-      ImGui::PopStyleVar();
-      if (ImGui::Button("OK", ImVec2(120,0))) {Load_mesh(gs); ImGui::CloseCurrentPopup();}
-      ImGui::SameLine();
-      if (ImGui::Button("Cancel", ImVec2(120,0))) {ImGui::CloseCurrentPopup();}
-      ImGui::EndPopup();
-    }
-
-    ImGui::SameLine();
-    if (ImGui::Button("Save scale-space")) Save_scale_space(gs);
-
-    ImGui::SeparatorText("Field");
-
-    ImGui::SeparatorText("Processing");
-
-    ImGui::SeparatorText("View");
-    if (ImGui::Checkbox("Show wireframe", &gs.show_wf)) {
-      if (gs.show_wf) gs.m.show_wireframe(true);
-      else gs.m.show_wireframe(false);
-      gs.m.updateGL();
-    } 
-    ImGui::SameLine();
-    if (ImGui::Checkbox("Show mesh", &gs.show_m)) {
-      if (gs.show_m) gs.m.show_mesh(true);
-      else gs.m.show_mesh(false);
-      gs.m.updateGL();
-    } 
-    if (ImGui::Checkbox("Show Scalar Field", &gs.show_sf)) {
-      if (gs.show_sf) {
-        gs.curr_clamp[0]=gs.clamp_limits[gs.selected_entry][0];
-        gs.curr_clamp[1]=gs.clamp_limits[gs.selected_entry][1];
-        gs.phi = Clamp_and_rescale_field(gs.fields[gs.selected_entry],gs.curr_clamp);
-        gs.phi.copy_to_mesh(gs.m);
-        gs.m.show_texture1D(TEXTURE_1D_HSV);
-      } else {
-        gs.m.show_poly_color();
-      }
-    } 
-    ImGui::SameLine();
-    if (ImGui::Checkbox("Show Critical Points", &gs.show_cp)) {
-      if (gs.show_cp) {
-        set_points(gs.m,gs.critical[gs.selected_entry],gs.points,gs.point_size*gs.point_multiplier);
-        draw_points(gs.points,gui);
-      } else 
-        remove_points(gs.points,gui);
-      // m.updateGL();
-    } 
-    if (ImGui::SliderFloat("Point size", &gs.point_multiplier, 0, 10.0, "%.1f")) {
-      if (gs.show_cp) {
-        set_points(gs.m,gs.critical[gs.selected_entry],gs.points,gs.point_size*gs.point_multiplier);
-        gs.m.updateGL();
-      }
-    }
-    if (ImGui::SliderFloat2("Clamp values", gs.curr_clamp, gs.f.minCoeff(), gs.f.maxCoeff(),"%.4f",ImGuiSliderFlags_Logarithmic)) {
-  //  if (ImGui::InputFloat2("Clamp values", curr_clamp,"%.4f")) {
-      if (gs.show_sf) {
-        gs.phi = Clamp_and_rescale_field(gs.fields[gs.selected_entry],gs.curr_clamp);
-        gs.phi.copy_to_mesh(gs.m);
-        gs.m.updateGL();
-      }
-    }
-    // if (ImGui::SliderInt("Choose level", &selected_entry, 0, nlevels - 1)) {
-    if (ImGui::InputInt("Level", &gs.selected_entry)) {
-      if (gs.selected_entry>=gs.nlevels) gs.selected_entry = gs.nlevels-1;
-      else {
-        if (gs.show_sf) {
-          gs.curr_clamp[0]=gs.clamp_limits[gs.selected_entry][0];
-          gs.curr_clamp[1]=gs.clamp_limits[gs.selected_entry][1];
-          gs.phi = Clamp_and_rescale_field(gs.fields[gs.selected_entry],gs.curr_clamp);
-          gs.phi.copy_to_mesh(gs.m);
-          gs.m.updateGL();
-        }
-        if (gs.show_cp) {
-          remove_points(gs.points,gui);
-          set_points(gs.m,gs.critical[gs.selected_entry],gs.points,gs.point_size*gs.point_multiplier);
-          draw_points(gs.points,gui);
-        }
-      }
-    }
-   };
-
-  gui.callback_mouse_left_click = [&](int modifiers) -> bool {
-    if (modifiers & GLFW_MOD_SHIFT) {
-      vec3d p;
-      vec2d click = gui.cursor_pos();
-      if (gui.unproject(click, p)) {
-        uint vid = gs.m.pick_vert(p);
-        cout << "Picked vertex " << vid << " field value " 
-              << std::setprecision(20) << gs.fields[gs.selected_entry][vid] 
-              << " at level " << gs.selected_entry << endl;
-        // m.vert_data(vid).color = Color::RED();
-        // m.updateGL();
-      }
-    }
-    return false;
-  };
-
-}
 
 
 
@@ -381,18 +273,19 @@ Eigen::VectorXd Random_field(const DrawableTrimesh<> & m)
 }
 
 
-
 //=============================== INPUT FIELD ==================================
-Eigen::VectorXd Generate_field(const DrawableTrimesh<> &m)
+void Generate_field(State & gs)
 {
-  Eigen::VectorXd buf;
-  // buf = Mean_curvature(m);
-  buf = gaussian_curvature(m);
-  // buf =  Laplacian_eigenfunction(m,100,10);
-  // buf = Coordinate(m,1);
-  // buf = Random_field(m);
-  // normalize_in_01(buf);
-  return buf;
+  switch(gs.current_field_method) {
+    case GAUSS: gs.f = gaussian_curvature(gs.m); break;
+    case MEAN: gs.f = Mean_curvature(gs.m); break;
+    case L_EIGEN: gs.f = Laplacian_eigenfunction(gs.m,gs.max_eigenfunctions,gs.selected_eigenfunction); break;
+    case COORDX:
+    case COORDY:
+    case COORDZ:
+    case RANDOM:
+    default: cout << "Bad case in switch - This shouldn't happen!\n";
+  }
 }
 
 void Set_clamp_limits(const Eigen::VectorXd &f, int sigma_multiplier, float cl[]) 
@@ -499,6 +392,138 @@ void print_statistics(const vector<vector<int>> &c)
 }
 
 
+//::::::::::::::::::::::::::::::::: GUI ::::::::::::::::::::::::::::::::::::::::::::::::
+
+GLcanvas Init_GUI()
+{
+  GLcanvas gui(1500,700);
+  gui.side_bar_width = 0.25;
+  gui.show_side_bar = true;
+  return gui;
+}
+
+void Setup_GUI_Callbacks(GLcanvas & gui, State &gs)
+{
+  gui.callback_app_controls = [&]() {
+    // Files
+    ImGui::SeparatorText("Files");
+    if (ImGui::Button("Load mesh")) {
+      if (gs.MESH_IS_LOADED) {
+        ImGui::OpenPopup("Load mesh?");
+      } else {
+        Load_mesh(gs);
+      }
+    }
+    // Modal popup for loading files
+    ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+    ImGui::SetNextWindowPos(center,ImGuiCond_Appearing,ImVec2(0.5f,0.5f));
+    if (ImGui::BeginPopupModal("Load mesh?", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse))
+    {
+      ImGui::Text("All data structures will be reset - Load mesh anyway?\n\n");
+      ImGui::Separator();           
+      static bool dont_ask_me_next_time = false;
+      ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0,0));
+      ImGui::Checkbox("Don't ask me next time", &dont_ask_me_next_time);
+      ImGui::PopStyleVar();
+      if (ImGui::Button("OK", ImVec2(120,0))) {Load_mesh(gs); ImGui::CloseCurrentPopup();}
+      ImGui::SameLine();
+      if (ImGui::Button("Cancel", ImVec2(120,0))) {ImGui::CloseCurrentPopup();}
+      ImGui::EndPopup();
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Save scale-space")) Save_scale_space(gs);
+
+    // Field
+    ImGui::SeparatorText("Field");
+    ImGui::Combo("Method",&gs.current_field_method,wrap_strings(gs.field_method_labels));
+    if (ImGui::Button("Generate field")) Generate_field(gs);
+
+    // Processing
+    ImGui::SeparatorText("Processing");
+
+    // View
+    ImGui::SeparatorText("View");
+    if (ImGui::Checkbox("Show wireframe", &gs.show_wf)) {
+      if (gs.show_wf) gs.m.show_wireframe(true);
+      else gs.m.show_wireframe(false);
+      gs.m.updateGL();
+    } 
+    ImGui::SameLine();
+    if (ImGui::Checkbox("Show mesh", &gs.show_m)) {
+      if (gs.show_m) gs.m.show_mesh(true);
+      else gs.m.show_mesh(false);
+      gs.m.updateGL();
+    } 
+    if (ImGui::Checkbox("Show Scalar Field", &gs.show_sf)) {
+      if (gs.show_sf) {
+        gs.curr_clamp[0]=gs.clamp_limits[gs.selected_entry][0];
+        gs.curr_clamp[1]=gs.clamp_limits[gs.selected_entry][1];
+        ScalarField phi = Clamp_and_rescale_field(gs.fields[gs.selected_entry],gs.curr_clamp);
+        phi.copy_to_mesh(gs.m);
+        gs.m.show_texture1D(TEXTURE_1D_HSV);
+      } else {
+        gs.m.show_poly_color();
+      }
+    } 
+    ImGui::SameLine();
+    if (ImGui::Checkbox("Show Critical Points", &gs.show_cp)) {
+      if (gs.show_cp) {
+        set_points(gs.m,gs.critical[gs.selected_entry],gs.points,gs.point_size*gs.point_multiplier);
+        draw_points(gs.points,gui);
+      } else 
+        remove_points(gs.points,gui);
+      // m.updateGL();
+    } 
+    if (ImGui::SliderFloat("Point size", &gs.point_multiplier, 0, 10.0, "%.1f")) {
+      if (gs.show_cp) {
+        set_points(gs.m,gs.critical[gs.selected_entry],gs.points,gs.point_size*gs.point_multiplier);
+        gs.m.updateGL();
+      }
+    }
+    if (ImGui::SliderFloat2("Clamp values", gs.curr_clamp, gs.f.minCoeff(), gs.f.maxCoeff(),"%.4f",ImGuiSliderFlags_Logarithmic)) {
+  //  if (ImGui::InputFloat2("Clamp values", curr_clamp,"%.4f")) {
+      if (gs.show_sf) {
+        ScalarField phi = Clamp_and_rescale_field(gs.fields[gs.selected_entry],gs.curr_clamp);
+        phi.copy_to_mesh(gs.m);
+        gs.m.updateGL();
+      }
+    }
+    // if (ImGui::SliderInt("Choose level", &selected_entry, 0, nlevels - 1)) {
+    if (ImGui::InputInt("Level", &gs.selected_entry)) {
+      if (gs.selected_entry>=gs.nlevels) gs.selected_entry = gs.nlevels-1;
+      else {
+        if (gs.show_sf) {
+          gs.curr_clamp[0]=gs.clamp_limits[gs.selected_entry][0];
+          gs.curr_clamp[1]=gs.clamp_limits[gs.selected_entry][1];
+          ScalarField phi = Clamp_and_rescale_field(gs.fields[gs.selected_entry],gs.curr_clamp);
+          phi.copy_to_mesh(gs.m);
+          gs.m.updateGL();
+        }
+        if (gs.show_cp) {
+          remove_points(gs.points,gui);
+          set_points(gs.m,gs.critical[gs.selected_entry],gs.points,gs.point_size*gs.point_multiplier);
+          draw_points(gs.points,gui);
+        }
+      }
+    }
+   };
+
+  gui.callback_mouse_left_click = [&](int modifiers) -> bool {
+    if (modifiers & GLFW_MOD_SHIFT) {
+      vec3d p;
+      vec2d click = gui.cursor_pos();
+      if (gui.unproject(click, p)) {
+        uint vid = gs.m.pick_vert(p);
+        cout << "Picked vertex " << vid << " field value " 
+              << std::setprecision(20) << gs.fields[gs.selected_entry][vid] 
+              << " at level " << gs.selected_entry << endl;
+        // m.vert_data(vid).color = Color::RED();
+        // m.updateGL();
+      }
+    }
+    return false;
+  };
+}
 
 
 //=============================== MAIN =========================================
@@ -532,8 +557,7 @@ int main(int argc, char **argv) {
 
   // GENERATE FIELD:::::::::::::::::::::::::::::::
   // change this function if you want to generate a different field
-  gs.f = Generate_field(gs.m);
-  gs.FIELD_IS_PRESENT = true;
+  Generate_field(gs);
 
   // COMPUTE DISCRETE SCALE SPACE:::::::::::::::::
   cout << "Computing discrete scale space: ";
